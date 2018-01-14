f21e111cae2ebb770b08b93367eae7baeb5089bf
Aaron Gooden <aagooden@me.com>
2018-01-13 19:05:49 -0500

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: [:show]
+  #the following line creates custom route for show...
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+
  
 # get 'pages/home'
 
